7ebb2cf64f745f57d22accf7fb653828777de599
thchoi0327 <mogtak02@naver.com>
2021-10-15 15:58:27 +0900

    [Add LoginPage]

---
+++ kakao_starter.py
@@ -0,0 +1,31 @@
+import sys
+from kakaoLogin2 import Ui_kakao #수정부분
+from PyQt5 import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+class kinwriter(QMainWindow, Ui_kakao): 
+    
+    def __init__(self):
+
+        super().__init__()
+
+        self.setupUi(self)
+        # self.timer = QTimer(self)
+        # self.timer.setSingleShot(False)
+        # self.timer.setInterval(5000) # in milliseconds, so 5000 = 5 seconds
+        # # self.timer.timeout.connect(self.start_Macro)
+        # self.timer.start()i0nscn2kdlr2k
+
+
+        #print(self.hasMouseTracking())
+
+
+        self.show()
+
+
+
+app = QApplication([])
+sn = kinwriter()
+QApplication.processEvents()
+sys.exit(app.exec_())
